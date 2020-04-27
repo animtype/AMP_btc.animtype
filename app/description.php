f68474f212817712b754b061118a075b50289991
Dr. Animtype <animtype@users.noreply.github.com>
2020-04-27 21:07:26 +0200

    inflation

--- app/description.php
+++ app/description.php
@@ -1,5 +1,5 @@
 	<?php $ver = '?ver=1' ?>
-	<title>Bitcoin Block Reward Halving Countdown [#inflation]</title>
+	<title>Bitcoin Block Reward Halving Countdown [#btc]</title>
 	<meta name="description" content="The Bitcoin halving decreases the number of new bitcoins generated per block. In most markets, lower BTC supply with steady demand usually leads to higher prices. Should we experience something similar to this year halving? What do you think?">
 	<meta name="robots" content="INDEX,FOLLOW">
 	<meta name="author" content="Dr.AnimType">
@@ -11,7 +11,7 @@
 	<meta name="msapplication-TileImage" content="<?php echo Base::instance()->get('url'); ?>mstile-270x270.png">
 
 	<meta property="fb:app_id" content="963224253825414">
-	<meta property="og:title" content="Bitcoin Block Reward Halving Countdown [#inflation]">
+	<meta property="og:title" content="Bitcoin Block Reward Halving Countdown [#btc]">
 	<meta property="og:description" content="The Bitcoin halving decreases the number of new bitcoins generated per block. In most markets, lower BTC supply with steady demand usually leads to higher prices. Should we experience something similar to this year halving? What do you think?">
 	<meta property="og:url" content="<?php echo Base::instance()->get('url'); ?>">
 	<meta property="og:site_name" content="BTC Halving">
@@ -20,6 +20,6 @@
 	<meta property="og:image:height" content="630" />
 	<meta property="og:type" content="website">
 	<meta name="twitter:card" content="summary_large_image">
-	<meta name="twitter:title" content="Bitcoin Block Reward Halving Countdown [#inflation]">
-	<meta name="twitter:description" content="The Bitcoin halving decreases the number of new bitcoins generated per block. In most markets, lower BTC supply with steady demand usually leads to higher prices. Should we experience something similar to this year halving? What do you think? #btc, #inflation, #bitcoin, #halving">
+	<meta name="twitter:title" content="Bitcoin Block Reward Halving Countdown [#btc]">
+	<meta name="twitter:description" content="The Bitcoin halving decreases the number of new bitcoins generated per block. In most markets, lower BTC supply with steady demand usually leads to higher prices. Should we experience something similar to this year halving? What do you think? #btc, #deflation, #bitcoin, #halving">
 	<meta name="twitter:image:src" content="<?php echo Base::instance()->get('url'); ?>share-img.jpg">
